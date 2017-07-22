a1a2731cca8fb581f49fd2513042592553888485
danilo <dnlsm@bk.ru>
2017-07-22 03:47:05 -0300

    Interceptor de headers do CORS; Interceptador de Autenticação; Routes para /login e /devices

--- www/api/apiServer.js
+++ www/api/apiServer.js
@@ -1,32 +1,84 @@
-const http = require('http')
-const port = 3000
-
-
-var mongoClient = require('mongodb').MongoClient
-
-var url = 'mongodb://localhost:3001/unper'
-
-mongoClient.connect(url, function (err,db){
-	if (err) throw err
-	console.log(db)
-})
+const router = require('./router')
+const url = require('url')
 var connections = []
 
+var devices = [{name: "LED", pin: 26, value: true},
+				{name: "LAMPADA", pin: 19, value: false}]
 
-var server  = http.createServer(function (req, res) {
-	res.write('HI')
+
+var auths = {	'dnlsm': '1998',
+				'eletro': '2016'}
+
+var authorized = {}
+var server = router(3000)
+
+server.get('/devices', function(req, res){
+		res.write(JSON.stringify(devices))
+		res.end()
+})
+
+server.post('/devices', function(req, res){
+		console.log('post')
+		res.end()
+})
+
+server.put('/devices', function(req, res){
+		console.log('put')
+		res.end()
+})
+
+server.options('/devices', function(req, res){
 	res.end()
 })
 
-server.listen(port, function () {
-	console.log("Api server started at port %s", port)
+server.get('/login', function (req, res){
+	console.log(req.headers.origin)
+
+	if (req.query){
+		var q = req.query
+		console.log(q)
+		if (auths[q.user] && (auths[q.user] === q.pwd)){
+			var id = "reqID=" + Math.random() * 65500;
+			authorized[id] = true
+			console.log(id)
+			res.setHeader('Set-Cookie', id + ';path=/');
+			res.statusCode = 200
+			return res.end()
+		}
+		res.statusCode = 404
+		return res.end()
+	}
+	else{
+		res.statusCode = 404
+		return res.end()
+	}
 })
-server.on('close',function(){
-	console.log('Server closed')
+
+
+
+server.interceptor(function(req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', req.headers.origin?req.headers.origin:'*')
+		res.setHeader('Access-Control-Allow-Credentials', 'true')
+		next()
 })
 
+server.interceptor(function(req, res, next){
+		
+		if (req.pathname === '/login') {
+			next()
+			return
+		}
+		else {
+			var cookie = req.headers.cookie
+			console.log(cookie)
+			if (cookie && authorized[cookie]){
 
+				next()
+				return
+			}
+			res.end()
+			return 'Error'
+		}
+})
 
-
-global.apiServer = server
 
